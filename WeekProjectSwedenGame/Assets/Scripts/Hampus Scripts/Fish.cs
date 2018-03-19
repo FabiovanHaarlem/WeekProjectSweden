b4fd69ea025315f6307b5cd1c3654547cb1d8b38
hampussen <hampuss22@gmail.com>
2018-03-19 20:15:51 +0100

    Fish script

---
+++ WeekProjectSwedenGame/Assets/Scripts/Hampus Scripts/Fish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Fish : MonoBehaviour {
+
+    private NavMeshAgent m_navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_navMeshAgent.SetDestination(new Vector3());
+    }
+    public void SpawnFish(Vector3 position,Vector3 TargetPosition)
+    {
+        transform.position = position;
+        m_navMeshAgent.SetDestination(TargetPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ WeekProjectSwedenGame/Assets/Scripts/Hampus Scripts/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+    private List<Fish> fishlist;
+
+	// Use this for initialization
+	void Start () {
+        fishlist = new List<Fish>();
+        for (int i = 0; i < 1; i++)
+        {
+            GameObject gameObject = Instantiate(Resources.Load<GameObject>("Prefabs/Fish"));
+            fishlist.Add(gameObject.GetComponent<Fish>());
+            gameObject.SetActive(false);
+        }
+	}
+    public void GetFish(Vector3 position, Vector3 TargetPosition)
+    {
+        for (int i = 0; i < fishlist.Count; i++)
+        {
+            if (fishlist[i].gameObject.activeInHierarchy != true)
+            {
+                fishlist[i].SpawnFish(position, TargetPosition);
+                break;
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
